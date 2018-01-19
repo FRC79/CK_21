da38ea5338d9079fa7740f7480f823c4d793db71
Noah <Noah@DESKTOP-5NH6TIT>
2018-01-19 11:51:00 -0500

    Various dimension constants

---
+++ src/org/usfirst/frc/team79/robot/RobotDimensions.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team79.robot;
+
+/**
+ * Contains any dimensions of the physical aspects of the robot, i.e. robot width. Comment with the unit of measurement
+ */
+public class RobotDimensions {
+	
+	/**Inches*/
+	public static final int WHEEL_WIDTH = 2;
+
+}
