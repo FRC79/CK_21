8587abae99dcbda2d7afdfb612c4d8bb3effd9be
xlash <xlash@10.162.244.49>
2018-02-07 17:44:11 -0500

    Waits a specified amount of seconds

---
+++ src/org/usfirst/frc/team79/robot/commands/WaitForIt.java
@@ -0,0 +1,26 @@
+package org.usfirst.frc.team79.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class WaitForIt extends Command{
+	
+	public Timer timer;
+	public double time;
+	
+	public WaitForIt(double time) {
+		this.time = time;
+	}
+	
+	@Override
+	protected void initialize() {
+		this.timer = new Timer();
+		this.timer.start();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return timer.hasPeriodPassed(time);
+	}
+
+}
