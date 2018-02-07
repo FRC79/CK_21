cadac94272cd8036e7cba2c20b48618980a9ca0a
xlash <xlash@10.162.244.49>
2018-02-07 11:36:29 -0500

    Added real wheel dimension and a ticks/rev variable

--- src/org/usfirst/frc/team79/robot/RobotDimensions.java
+++ src/org/usfirst/frc/team79/robot/RobotDimensions.java
@@ -6,10 +6,13 @@ package org.usfirst.frc.team79.robot;
 public class RobotDimensions {
 	
 	/**The width of the drive train wheels inches*/
-	public static final double WHEEL_WIDTH = 2;
+	public static final double WHEEL_WIDTH = 1;
 	/**The diameter of the drive train wheels in inches*/
-	public static final double WHEEL_DIAMETER = 12;
-	/**The circumference of the wheel in feet*/
-	public static final double WHEEL_CIRCUMFERENCE = Math.PI*(WHEEL_DIAMETER/12d);
+	public static final double WHEEL_DIAMETER = 6;
+	/**The circumference of the wheel in inches*/
+	public static final double WHEEL_CIRCUMFERENCE = Math.PI*(WHEEL_DIAMETER);
+	
+	/**The amount of encoder ticks per 1 revolution*/
+	public static final int TICKS_PER_REV = 1200; //TODO
 
 }
