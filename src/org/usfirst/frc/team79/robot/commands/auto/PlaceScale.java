705b92ea898a12381642274907732aa30e1e94ed
xlash <xlash@10.162.244.49>
2018-02-07 17:46:25 -0500

    Started autonomous commands

--- src/org/usfirst/frc/team79/robot/commands/auto/PlaceScale.java
+++ src/org/usfirst/frc/team79/robot/commands/auto/PlaceScale.java
@@ -1,25 +1,25 @@
 package org.usfirst.frc.team79.robot.commands.auto;
 
+import org.usfirst.frc.team79.robot.commands.IntakeOut;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  * Autonomous command for placing a Power "Cube" on the center scale
  *
  */
-public class PlaceScale extends CommandGroup{
+public class PlaceScale extends CommandGroup {
 	
-	public PlaceScale() {
-		
+	/**
+	 * Attempts to autonomously place a block on the scale during autonomous period
+	 * @param wall The left or right side of the field
+	 * @param side A single character representing which scale is ours
+	 */
+	public PlaceScale(String wall, char side) {
+		this.addSequential(new RunMotionProfile(wall+"WallScale"+side));
+		this.addSequential(new WaitCommand(0.5));
+		this.addSequential(new IntakeOut(2));
 	}
 	
-	@Override
-	public void execute() {
-		
-	}
-
-	@Override
-	protected boolean isFinished() {
-		return false;
-	}
-
 }
--- src/org/usfirst/frc/team79/robot/commands/auto/PlaceSwitch.java
+++ src/org/usfirst/frc/team79/robot/commands/auto/PlaceSwitch.java
@@ -1,6 +1,9 @@
 package org.usfirst.frc.team79.robot.commands.auto;
 
+import org.usfirst.frc.team79.robot.commands.IntakeOut;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.command.WaitCommand;
 
 /**
  * Autonomous command for placing a Power "Cube" on the alliance switch
@@ -8,18 +11,15 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class PlaceSwitch extends CommandGroup{
 	
-	public PlaceSwitch() {
-		
-	}
-	
-	@Override
-	public void execute() {
-		
-	}
-
-	@Override
-	protected boolean isFinished() {
-		return false;
+	/**
+	 * Attempts to autonomously place a block on the switch during autonomous period
+	 * @param wall The left or right side of the field
+	 * @param side A single character representing which switch is ours
+	 */
+	public PlaceSwitch(String wall, char side) {
+		this.addSequential(new RunMotionProfile(wall+"WallSwitch"+side));
+		this.addSequential(new WaitCommand(0.5));
+		this.addSequential(new IntakeOut(2));
 	}
 
 }
