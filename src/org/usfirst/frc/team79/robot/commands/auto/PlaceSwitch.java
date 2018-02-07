6d2c21285394ee42b86c8a708b78f9d0f1f54b17
xlash <xlash@10.162.244.49>
2018-02-07 18:12:48 -0500

    Lifts the elevator

--- src/org/usfirst/frc/team79/robot/commands/auto/PlaceSwitch.java
+++ src/org/usfirst/frc/team79/robot/commands/auto/PlaceSwitch.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team79.robot.commands.auto;
 
 import org.usfirst.frc.team79.robot.commands.IntakeOut;
+import org.usfirst.frc.team79.robot.commands.LiftElevator;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.WaitCommand;
@@ -17,6 +18,7 @@ public class PlaceSwitch extends CommandGroup{
 	 * @param side A single character representing which switch is ours
 	 */
 	public PlaceSwitch(String wall, char side) {
+		this.addParallel(new LiftElevator());
 		this.addSequential(new RunMotionProfile(wall+"WallSwitch"+side));
 		this.addSequential(new WaitCommand(0.5));
 		this.addSequential(new IntakeOut(2));
