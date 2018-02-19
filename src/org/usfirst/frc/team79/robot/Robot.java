f885d340903fc4b0c7f1b5ed4e810bc3a6165f56
xlash <xlash@10.162.244.49>
2018-02-19 16:00:29 -0500

    Added autonomous based on encoders

--- src/org/usfirst/frc/team79/robot/Robot.java
+++ src/org/usfirst/frc/team79/robot/Robot.java
@@ -9,6 +9,9 @@ package org.usfirst.frc.team79.robot;
 
 import org.usfirst.frc.team79.robot.subsystems.Climber;
 import org.usfirst.frc.team79.robot.commands.auto.CrossAuto;
+import org.usfirst.frc.team79.robot.commands.auto.PlaceEitherSwitch;
+import org.usfirst.frc.team79.robot.commands.auto.PlaceScale;
+import org.usfirst.frc.team79.robot.commands.auto.PlaceSwitch;
 import org.usfirst.frc.team79.robot.commands.auto.RunMotionProfile;
 import org.usfirst.frc.team79.robot.pathfinding.MotionProfileManager;
 import org.usfirst.frc.team79.robot.pid.GyroPIDController;
@@ -25,6 +28,7 @@ import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import jaci.pathfinder.Waypoint;
+import org.usfirst.frc.team79.robot.util.Side;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -40,17 +44,19 @@ public class Robot extends TimedRobot {
 	public static Elevator elevator;
 	public static Intake intake;
 	public static Climber climber;
-	
+
 	public static GyroPIDController gyroPID;
-	
+
 	public static CameraServer camServer;
 
 	Command autoCommand;
-	SendableChooser<String> chooser = new SendableChooser<>();
+	SendableChooser<String> mpChooser = new SendableChooser<>();
+	SendableChooser<Side> wallChooser = new SendableChooser<>();
+	SendableChooser<String> autoChooser = new SendableChooser<>();
 
 	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
+	 * This function is run when the robot is first started up and should be used
+	 * for any initialization code.
 	 */
 	@Override
 	public void robotInit() {
@@ -61,20 +67,31 @@ public class Robot extends TimedRobot {
 
 		gyroPID = new GyroPIDController();
 		oi = new OI();
-		
+
 		UsbCamera cam = new UsbCamera("cam0", 0);
 		cam.setBrightness(70);
 		camServer = CameraServer.getInstance();
 		camServer.addCamera(cam);
-		
-		chooser.addObject("Cross Auto Line", "CrossAuto");
-		chooser.addObject("Left Wall : Scale", "LeftWallScale");
-		chooser.addObject("Left Wall : Switch", "LeftWallSwitch");
-		chooser.addObject("Right Wall : Scale", "RightWallScale");
-		chooser.addObject("Right Wall : Switch", "RightWallSwitch");
-		chooser.addObject("Middle Wall : Switch", "MiddleWallSwitch");
-		
-		SmartDashboard.putData("Auto mode", chooser);
+
+		wallChooser.addObject("Left Wall", Side.LEFT);
+		wallChooser.addObject("Right Wall", Side.RIGHT);
+		wallChooser.addObject("Middle Wall", Side.MIDDLE);
+
+		autoChooser.addObject("Place Scale", "Scale");
+		autoChooser.addObject("Place Switch", "Switch");
+		autoChooser.addObject("Either, priority Scale", "EitherScale");
+		autoChooser.addObject("Either, priority Switch", "EitherSwitch");
+		autoChooser.addObject("Cross Line", "CrossAuto");
+
+//		mpChooser.addObject("Cross Auto Line", "CrossAuto");
+//		mpChooser.addObject("Left Wall : Scale", "LeftWallScale");
+//		mpChooser.addObject("Left Wall : Switch", "LeftWallSwitch");
+//		mpChooser.addObject("Right Wall : Scale", "RightWallScale");
+//		mpChooser.addObject("Right Wall : Switch", "RightWallSwitch");
+//		mpChooser.addObject("Middle Wall : Switch", "MiddleWallSwitch");
+
+		SmartDashboard.putData("Starting Location", wallChooser);
+		SmartDashboard.putData("Autonomous Mode", autoChooser);
 		SmartDashboard.putData(driveTrain.gyro);
 		SmartDashboard.putData(gyroPID);
 		System.out.println("~~~Robot initialization complete!~~~");
@@ -82,9 +99,9 @@ public class Robot extends TimedRobot {
 	}
 
 	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
+	 * This function is called once each time the robot enters Disabled mode. You
+	 * can use it to reset any subsystem information you want to clear when the
+	 * robot is disabled.
 	 */
 	@Override
 	public void disabledInit() {
@@ -98,30 +115,75 @@ public class Robot extends TimedRobot {
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString code to get the auto name from the text box below the Gyro
+	 * between different autonomous modes using the dashboard. The sendable chooser
+	 * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+	 * remove all of the chooser code and uncomment the getString code to get the
+	 * auto name from the text box below the Gyro
 	 *
-	 * <p>You can add additional auto modes by adding additional commands to the
-	 * chooser code above (like the commented example) or additional comparisons
-	 * to the switch structure below with additional strings & commands.
+	 * <p>
+	 * You can add additional auto modes by adding additional commands to the
+	 * chooser code above (like the commented example) or additional comparisons to
+	 * the switch structure below with additional strings & commands.
 	 */
 	@Override
 	public void autonomousInit() {
-		String auto = chooser.getSelected();
-		//Lets the robot know which platform for the switches and scale is ours.
+		autoEnc();
+	}
+
+	// For if we ever get motion profiling working
+	private void autoMP() {
+		String auto = mpChooser.getSelected();
+		// Lets the robot know which platform for the switches and scale is ours.
 		String fmsMessage = DriverStation.getInstance().getGameSpecificMessage();
-		if(auto.contains("Scale")) {
+		if (auto.contains("Scale")) {
 			auto += fmsMessage.charAt(1);
-		}else if(auto.contains("Switch")){
+		} else if (auto.contains("Switch")) {
 			auto += fmsMessage.charAt(0);
-		}else {
+		} else {
 			autoCommand = new CrossAuto();
 			autoCommand.start();
 			return;
 		}
 		autoCommand = new RunMotionProfile(auto);
+		autoCommand.start();
+	}
+
+	// Using encoders and gyro to get around
+	private void autoEnc() {
+		String auto = autoChooser.getSelected();
+		Side wall = wallChooser.getSelected();
+		String fmsMessage = DriverStation.getInstance().getGameSpecificMessage().substring(0, 2);
+		Side swtch = Side.fromChar(fmsMessage.charAt(0));
+		Side scale = Side.fromChar(fmsMessage.charAt(1));
+		if(wall==Side.MIDDLE) {
+			autoCommand = new PlaceEitherSwitch(swtch);
+			return;
+		}
+		if (auto.equals("Scale")) {
+			autoCommand = new PlaceScale(scale, wall);
+		} else if (auto.equals("Switch")) {
+			// If the switch is on the opposite side, the robot will just cross auto
+			autoCommand = new PlaceSwitch(swtch, wall);
+		} else if (auto.contains("Either")) {
+			if (fmsMessage.contains("" + wall.toChar())) {
+				if (auto.contains("Scale")) {
+					if (scale == wall) {
+						autoCommand = new PlaceScale(scale, wall);
+					} else if (swtch == wall) {
+						autoCommand = new PlaceSwitch(swtch, wall);
+					}
+				} else {
+					if (swtch == wall) {
+						autoCommand = new PlaceSwitch(swtch, wall);
+					} else if (scale == wall) {
+						autoCommand = new PlaceScale(scale, wall);
+					}
+				}
+			}
+		}
+
+		if (autoCommand == null)
+			autoCommand = new CrossAuto();
 		autoCommand.start();
 	}
 
@@ -149,17 +211,22 @@ public class Robot extends TimedRobot {
 		SmartDashboard.putNumber("Left Speed", driveTrain.getLeftVel());
 		Scheduler.getInstance().run();
 	}
-	
+
 	@Override
 	public void testInit() {
-		MotionProfileManager.generate("LeftWallScaleL", new Waypoint(0,0,0), new Waypoint(208, 0, 0), new Waypoint(266.148, -17, 0));
-		MotionProfileManager.generate("LeftWallSwitchL", new Waypoint(0,0,0), new Waypoint(107.18, -42, 0));
-		MotionProfileManager.generate("LeftWallScaleR", new Waypoint(0, 0, 0), new Waypoint(150, 0, 0), new Waypoint(208, -96.31, Math.toRadians(-90)), new Waypoint(266.148, -177.31, 0));
-		MotionProfileManager.generate("RightWallScaleR", new Waypoint(0, 0, 0), new Waypoint(208, 0, 0), new Waypoint(266.148, -17, 0));
-		MotionProfileManager.generate("RightWallScaleL", new Waypoint(0, 0, 0), new Waypoint(208, 0, 0), new Waypoint(0, -96.31, Math.toRadians(-90)), new Waypoint(58.148, 81, 90));
+		MotionProfileManager.generate("LeftWallScaleL", new Waypoint(0, 0, 0), new Waypoint(208, 0, 0),
+				new Waypoint(266.148, -17, 0));
+		MotionProfileManager.generate("LeftWallSwitchL", new Waypoint(0, 0, 0), new Waypoint(107.18, -42, 0));
+		MotionProfileManager.generate("LeftWallScaleR", new Waypoint(0, 0, 0), new Waypoint(150, 0, 0),
+				new Waypoint(208, -96.31, Math.toRadians(-90)), new Waypoint(266.148, -177.31, 0));
+		MotionProfileManager.generate("RightWallScaleR", new Waypoint(0, 0, 0), new Waypoint(208, 0, 0),
+				new Waypoint(266.148, -17, 0));
+		MotionProfileManager.generate("RightWallScaleL", new Waypoint(0, 0, 0), new Waypoint(208, 0, 0),
+				new Waypoint(0, -96.31, Math.toRadians(-90)), new Waypoint(58.148, 81, 90));
 		MotionProfileManager.generate("RightWallSwitchR", new Waypoint(0, 0, 0), new Waypoint(107.18, 42, 0));
 		MotionProfileManager.generate("MiddleWallSwitchR", new Waypoint(0, 0, 0), new Waypoint(108.55, 0, 0));
-		MotionProfileManager.generate("Test", new Waypoint(0,0,0), new Waypoint(12, 0, 0)); //A test profile for tuning PID loops
+		MotionProfileManager.generate("Test", new Waypoint(0, 0, 0), new Waypoint(12, 0, 0)); // A test profile for
+																								// tuning PID loops
 	}
 
 	/**
