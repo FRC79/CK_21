0722971930a857c88056308247e780d83c79a43f
xlash <xlash@10.162.244.49>
2018-02-19 12:03:54 -0500

    Move straight for specified amount

---
+++ src/org/usfirst/frc/team79/robot/commands/auto/DriveDistance.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team79.robot.commands.auto;
+
+import org.usfirst.frc.team79.robot.Robot;
+import org.usfirst.frc.team79.robot.RobotDimensions;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveDistance extends Command{
+	
+	public double distance;
+
+	/**
+	 * Drive straight for a specified distance
+	 * @param distance in inches
+	 */
+	public DriveDistance(double distance) {
+		this.distance = distance;
+	}
+	
+	@Override
+	protected void initialize() {
+		Robot.driveTrain.resetEncoders();
+		Robot.driveTrain.gyro.reset();
+	}
+	
+	@Override
+	protected void execute() {
+		Robot.driveTrain.driveStraight(0.6, 0);
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		return Robot.driveTrain.getRightInches() >= distance;
+	}
+
+}
