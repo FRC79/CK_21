d85050044219a9b40f42b4c2aad0743fac5656b5
Team 79 Krunch <Team 79 Krunch@LAPTOP-LJN0O9TD>
2018-02-19 11:23:39 -0500

    Switched Intake controls to driver controller

--- src/org/usfirst/frc/team79/robot/OI.java
+++ src/org/usfirst/frc/team79/robot/OI.java
@@ -23,10 +23,11 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 public class OI {
 	
 	public Joystick drive = new Joystick(0);
-	public Button intakeIn = new JoystickButton(drive,7); 
-	public Button intakeOut = new JoystickButton(drive,8);
+	
 
 	public Joystick operator = new Joystick(1);
+	public Button intakeIn = new JoystickButton(operator,7); 
+	public Button intakeOut = new JoystickButton(operator ,8);
 	
 	public OI() {
 		intakeIn.whileHeld(new IntakeIn());
